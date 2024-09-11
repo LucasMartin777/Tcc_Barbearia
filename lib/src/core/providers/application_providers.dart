9536b09d48ec85a15b4ec32eb46330cce2ad786e
Lucas Martin Bogner Chiquito <lucaschiquito2@hotmail.com>
2024-09-11 15:59:42 -0300

    Refactories
    Refactorie feito nas constants de imagem e user service provider/riverpod

--- lib/src/core/providers/application_providers.dart
+++ lib/src/core/providers/application_providers.dart
@@ -1,6 +1,8 @@
 import 'package:barbearia_tcc/src/core/restClient/rest_clent.dart';
 import 'package:barbearia_tcc/src/repositories/user/user_repository.dart';
 import 'package:barbearia_tcc/src/repositories/user/user_repository_impl.dart';
+import 'package:barbearia_tcc/src/services/user_login/user_login_service.dart';
+import 'package:barbearia_tcc/src/services/user_login/user_login_service_impl.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'application_providers.g.dart';
 
@@ -9,3 +11,6 @@ RestClient restClient(RestClientRef ref) => RestClient();
 @Riverpod(keepAlive: true)
 UserRepository userRepository(UserRepositoryRef ref) =>
     UserRepositoryImpl(restClient: ref.read(restClientProvider));
+@Riverpod(keepAlive: true)
+UserLoginService userLoginService(UserLoginServiceRef ref) =>
+    UserLoginServiceImpl(userRepository: ref.read(userRepositoryProvider));
--- lib/src/features/splash/splash_page.dart
+++ lib/src/features/splash/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:barbearia_tcc/src/core/ui/constants.dart';
 import 'package:barbearia_tcc/src/features/auth/login/login_page.dart';
 import 'package:flutter/material.dart';
 
@@ -34,7 +35,7 @@ class _SplashPageState extends State<SplashPage> {
         decoration: const BoxDecoration(
           image: DecorationImage(
               image: AssetImage(
-                'assets/images/background_image_chair.jpg',
+                ImageConstants.backgroundChair,
               ),
               opacity: 0.2,
               fit: BoxFit.cover),
@@ -67,7 +68,7 @@ class _SplashPageState extends State<SplashPage> {
                 height: _logoAnimatedHeight,
                 curve: Curves.linearToEaseOut,
                 child: Image.asset(
-                  'assets/images/newlogo2.png',
+                  ImageConstants.imageLogo,
                   fit: BoxFit.cover,
                 )),
           ),
